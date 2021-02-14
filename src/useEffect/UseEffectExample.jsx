ef39b23f11a80552f89657d27b7c5f48067d6074
IsabelKe <45696243+IsabelKe@users.noreply.github.com>
2021-02-14 16:33:09 -0700

    add comments

--- src/useEffect/UseEffectExample.jsx
+++ src/useEffect/UseEffectExample.jsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 
 export default function UseEffectExample() {
-  //const url = "https://api.github.com/users/QuincyLarson";
-  document.title = "Use useEffect to change DOM";
-
+  //the callback function will only be executed once after
+  //the first rendering
   useEffect(() => {
+    //update the DOM
+    document.title = "Use useEffect to change DOM";
+    //url
     const url = "https://api.github.com/users/QuincyLarson";
+    //fetch user QuincyLarson
     fetch(url)
       .then((resp) => {
         return resp.json();
       })
       .then((user) => {
+        //print it out
         console.log(user.login);
       });
-  });
+  }, []);
 
   return (
     <>
